bbd097a6a28b274990576bbbb86802522335778a
Paul Duffy <paul.duffy@netan.solutions>
2018-12-16 12:26:49 +0000

    Init commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { startLogin } from '../actions/auth'
+
+export const LoginPage = ({ startLogin }) => (
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">boilerplate-react-scss</h1>
+            <p>Rename the stuff here before starting.</p>
+            <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
+    </div>
+)
+
+const mapDispatchToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
+})
+
+export default connect(undefined, mapDispatchToProps)(LoginPage)
